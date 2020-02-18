16a7abcfbf5faf7309571101bdc68d25281a940b
brettraz <brett.rasmussen@kellogg.northwestern.edu>
2020-02-18 03:01:59 +0000

    geocoder class

--- where2.rb
+++ where2.rb
@@ -4,6 +4,9 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-  # lat: ± 90
-  # long: ± 180
+  @lat= rand(-90.0000..90.0000)
+  @long= rand(-180.0000..180.0000)
+  @lat_long =  "#{@lat} #{@long}"
+  view "where2"
+  
 end
